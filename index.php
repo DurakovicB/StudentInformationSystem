7e9e25d20b54e6b21addd05f827ee1d624ea86fd
DurakovicB <bilal.durakovic@stu.ibu.ba>
2022-03-14 16:20:40 +0100

    slight changes

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 require 'vendor/autoload.php';
 
-Flight::route("/", function()
-{
-  echo 'Hello world!#2';
-});
-Flight::start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "WebProgrammer";
+$password = "WebProgrammer";
+$dbname = "systeminformationsystem";
+$table = $_REQUEST['table'];
+
+  $conn = new PDO("mysql:host=$servername;$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully <br>";
+
+  $select = $conn->prepare("SELECT * FROM $dbname.$table ;");
+  $select->execute();
+  $result = $select->fetchAll(PDO::FETCH_ASSOC);
+  print_r($result);
+
+
+
  ?>
