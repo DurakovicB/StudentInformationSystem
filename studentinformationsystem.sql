c7c1d659a7edc2f858eb3df6941b9f52be2dce61
Bilal Durakovic <durakovic.bilal@gmail.com>
2023-07-26 15:01:26 +0200

    working on timetable

--- studentinformationsystem.sql
+++ studentinformationsystem.sql
@@ -1,149 +1,197 @@
+CREATE DATABASE  IF NOT EXISTS `studentinformationsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `studentinformationsystem`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: studentinformationsystem
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- ----------------------------
--- Table structure for course
--- ----------------------------
+--
+-- Table structure for table `course`
+--
+
 DROP TABLE IF EXISTS `course`;
-CREATE TABLE `course`  (
-  `id` int(40) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `professor_id` int(40) UNSIGNED NULL DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `course` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `professor_id` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `professor`(`professor_id`) USING BTREE,
+  KEY `professor` (`professor_id`) USING BTREE,
   CONSTRAINT `professor` FOREIGN KEY (`professor_id`) REFERENCES `professor` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of course
--- ----------------------------
-INSERT INTO `course` VALUES (1, 'Calculus II', 'Second installment of calculus', 5);
-INSERT INTO `course` VALUES (2, 'Calculus I', 'Math basics', 5);
-INSERT INTO `course` VALUES (3, 'Intro to Web ', 'Basics of Web Development', 2);
-INSERT INTO `course` VALUES (4, 'Intro to Mobile', 'Mobile programming - Android Studio / Java', 2);
-INSERT INTO `course` VALUES (5, 'Discrete Maths', 'Intermediatery mathematics, including cryptography', 2);
-INSERT INTO `course` VALUES (6, 'Database Design', 'Intermediary course on database theory and MySQL', 1);
+--
+-- Dumping data for table `course`
+--
 
--- ----------------------------
--- Table structure for notification
--- ----------------------------
+LOCK TABLES `course` WRITE;
+/*!40000 ALTER TABLE `course` DISABLE KEYS */;
+INSERT INTO `course` VALUES (1,'Calculus II','Second installment of calculus',5),(2,'Calculus I','Math basics',5),(3,'Intro to Web ','Basics of Web Development',2),(4,'Intro to Mobile','Mobile programming - Android Studio / Java',2),(5,'Discrete Maths','Intermediatery mathematics, including cryptography',2),(6,'Database Design','Intermediary course on database theory and MySQL',1);
+/*!40000 ALTER TABLE `course` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notification`
+--
+
 DROP TABLE IF EXISTS `notification`;
-CREATE TABLE `notification`  (
-  `id` int(40) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
-  `created_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notification` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_bin ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of notification
--- ----------------------------
-INSERT INTO `notification` VALUES (1, 'Regarding exam registration', 'Dear Students, on the LMS you can find a video tutorial on how to register for your exams. Please be aware that students will not be able to do exams that they haven\'t previously registered for.', '2023-04-12 15:39:48');
-INSERT INTO `notification` VALUES (2, 'Happy Holidays', 'The staff of the University wishes you happy holidays. We hope to see you soon!', '2023-04-17 15:17:33');
-INSERT INTO `notification` VALUES (3, 'Erasmus Information', 'Information about upcoming erasmus programs will be available on our website. Be sure to check it regularly.', '2023-05-17 15:21:01');
+--
+-- Dumping data for table `notification`
+--
 
--- ----------------------------
--- Table structure for professor
--- ----------------------------
+LOCK TABLES `notification` WRITE;
+/*!40000 ALTER TABLE `notification` DISABLE KEYS */;
+INSERT INTO `notification` VALUES (1,'Regarding exam registration','Dear Students, on the LMS you can find a video tutorial on how to register for your exams. Please be aware that students will not be able to do exams that they haven\'t previously registered for.','2023-04-12 13:39:48'),(2,'Happy Holidays','The staff of the University wishes you happy holidays. We hope to see you soon!','2023-04-17 13:17:33'),(3,'Erasmus Information','Information about upcoming erasmus programs will be available on our website. Be sure to check it regularly.','2023-05-17 13:21:01'),(4,'Happy New Hijri Year','We wish all students the best for the upcoming holidays regarding new Hijri year.','2023-07-22 11:50:58');
+/*!40000 ALTER TABLE `notification` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor`
+--
+
 DROP TABLE IF EXISTS `professor`;
-CREATE TABLE `professor`  (
-  `id` int(40) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `fullname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `dateofbirth` date NULL DEFAULT NULL,
-  `gender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `professor` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `fullname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `dateofbirth` date DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of professor
--- ----------------------------
-INSERT INTO `professor` VALUES (1, 'dzelilam@gmail.com', 'Dzelila Mehanovic', '061827455', '1990-12-12', 'Female');
-INSERT INTO `professor` VALUES (2, 'dinokeco@ibu.edu.ba', 'Dino Keco', '062234123', '1973-04-12', 'Male');
-INSERT INTO `professor` VALUES (4, 'elma.avdic@ibu.edu.ba', 'Elma Avdic', '061332412', '1975-04-01', 'Female');
-INSERT INTO `professor` VALUES (5, 'saidasultanic@stu.ibu.edu.ba', 'Saida Sultanic', '062445611', '1967-02-07', 'Female');
-INSERT INTO `professor` VALUES (7, 'becirisakovic@ibu.edu.ba', 'Becir Isakovic', '06222435', '1996-06-09', 'male');
+--
+-- Dumping data for table `professor`
+--
 
--- ----------------------------
--- Table structure for student
--- ----------------------------
+LOCK TABLES `professor` WRITE;
+/*!40000 ALTER TABLE `professor` DISABLE KEYS */;
+INSERT INTO `professor` VALUES (1,'dzelilam@gmail.com','Dzelila Mehanovic','061827455','1990-12-12','Female'),(2,'dinokeco@ibu.edu.ba','Dino Keco','062234123','1973-04-12','Male'),(4,'elma.avdic@ibu.edu.ba','Elma Avdic','061332412','1975-04-01','Female'),(5,'saidasultanic@stu.ibu.edu.ba','Saida Sultanic','062445611','1967-02-07','Female'),(7,'becirisakovic@ibu.edu.ba','Becir Isakovic','06222435','1996-06-09','male');
+/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
 DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student`  (
-  `id` int(40) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `fullname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `gender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `fullname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `gender` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of student
--- ----------------------------
-INSERT INTO `student` VALUES (1, 'ahmed.durakovic@gmail.ba', 'Ahmed Durakovic', '1231231231', 'Male');
-INSERT INTO `student` VALUES (2, 'melisa22@gmail.com', 'Melisa Brulic', '44', 'female');
-INSERT INTO `student` VALUES (3, 'merisa4@gmai.com', 'Merisa Brrulic', '4122', 'female');
-INSERT INTO `student` VALUES (4, 'bilal.durakovic@stu.ibu.edu.ba', 'Bilal Durakovic', '062030834', 'male');
-INSERT INTO `student` VALUES (5, 'farisrizvanovic@gmail.com', 'Faris Rizvanovic', '322412', 'male');
+--
+-- Dumping data for table `student`
+--
 
--- ----------------------------
--- Table structure for student_courses
--- ----------------------------
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES (1,'ahmed.durakovic@gmail.ba','Ahmed Durakovic','1231231231','Male'),(2,'melisa22@gmail.com','Melisa Brulic','44','female'),(3,'merisa4@gmai.com','Merisa Brrulic','4122','female'),(4,'bilal.durakovic@stu.ibu.edu.ba','Bilal Durakovic','062030834','male'),(5,'farisrizvanovic@gmail.com','Faris Rizvanovic','322412','male');
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student_courses`
+--
+
 DROP TABLE IF EXISTS `student_courses`;
-CREATE TABLE `student_courses`  (
-  `student_id` int(40) UNSIGNED NOT NULL,
-  `course_id` int(40) UNSIGNED NOT NULL,
-  `percentage_total_amount` int(40) NULL DEFAULT NULL,
-  `percentage_acquired` int(40) NULL DEFAULT NULL,
-  `grade_title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `id` int(40) UNSIGNED NOT NULL AUTO_INCREMENT,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_courses` (
+  `student_id` int unsigned NOT NULL,
+  `course_id` int unsigned NOT NULL,
+  `percentage_total_amount` int DEFAULT NULL,
+  `percentage_acquired` int DEFAULT NULL,
+  `grade_title` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `course`(`course_id`) USING BTREE,
-  INDEX `student`(`student_id`) USING BTREE,
+  KEY `course` (`course_id`) USING BTREE,
+  KEY `student` (`student_id`) USING BTREE,
   CONSTRAINT `course` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `student` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of student_courses
--- ----------------------------
-INSERT INTO `student_courses` VALUES (1, 2, 30, 60, 'Midterm Exam', 1);
-INSERT INTO `student_courses` VALUES (1, 2, 40, 75, 'Project', 2);
-INSERT INTO `student_courses` VALUES (1, 2, 30, 55, 'Final', 3);
-INSERT INTO `student_courses` VALUES (1, 3, 10, 100, 'Quiz 1', 4);
-INSERT INTO `student_courses` VALUES (1, 1, 5, 40, 'Quiz 1', 7);
-INSERT INTO `student_courses` VALUES (1, 1, 30, 70, 'midterm', 19);
-INSERT INTO `student_courses` VALUES (1, 2, 10, 85, 'quiz', 20);
-INSERT INTO `student_courses` VALUES (1, 3, 30, 60, 'final', 21);
-INSERT INTO `student_courses` VALUES (2, 1, 30, 80, 'midterm', 22);
-INSERT INTO `student_courses` VALUES (2, 3, 10, 70, 'quiz', 23);
-INSERT INTO `student_courses` VALUES (3, 1, 30, 65, 'final', 24);
-INSERT INTO `student_courses` VALUES (3, 2, 30, 90, 'midterm', 25);
-INSERT INTO `student_courses` VALUES (4, 1, 30, 75, 'midterm', 26);
-INSERT INTO `student_courses` VALUES (4, 3, 10, 80, 'quiz', 27);
-INSERT INTO `student_courses` VALUES (5, 2, 10, 75, 'final', 28);
-INSERT INTO `student_courses` VALUES (5, 3, 30, 70, 'midterm', 29);
+--
+-- Dumping data for table `student_courses`
+--
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
+LOCK TABLES `student_courses` WRITE;
+/*!40000 ALTER TABLE `student_courses` DISABLE KEYS */;
+INSERT INTO `student_courses` VALUES (1,2,30,60,'Midterm Exam',1),(1,2,40,75,'Project',2),(1,2,30,55,'Final',3),(1,3,10,100,'Quiz 1',4),(1,1,5,40,'Quiz 1',7),(1,1,30,70,'midterm',19),(1,2,10,85,'quiz',20),(1,3,30,60,'final',21),(2,1,30,80,'midterm',22),(2,3,10,70,'quiz',23),(3,1,30,65,'final',24),(3,2,30,90,'midterm',25),(4,1,30,75,'midterm',26),(4,3,10,80,'quiz',27),(5,2,10,75,'final',28),(5,3,30,70,'midterm',29);
+/*!40000 ALTER TABLE `student_courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `student_id` int(40) UNSIGNED NULL DEFAULT NULL,
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `student_id` int unsigned DEFAULT NULL,
+  `professor_id` int DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (1, 'ahmed@ibu.edu.ba', '202cb962ac59075b964b07152d234b70', 1);
-INSERT INTO `users` VALUES (2, 'bilal@ibu.edu.ba', '202cb962ac59075b964b07152d234b70', 4);
+--
+-- Dumping data for table `users`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'ahmed@ibu.edu.ba','202cb962ac59075b964b07152d234b70',1,NULL),(2,'bilal@ibu.edu.ba','202cb962ac59075b964b07152d234b70',4,NULL),(3,'dino.keco@ibu.edu.ba','202cb962ac59075b964b07152d234b70',0,2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-26 14:59:04
